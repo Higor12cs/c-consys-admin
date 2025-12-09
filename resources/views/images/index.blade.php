a42c6e655e3a6d9faa9ca0bfcadd3a5de0a03fce
Higor Carneiro <higor12cs@gmail.com>
2025-12-09 13:56:06 -0300

    image layout fix

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -140,8 +140,17 @@
 
         <div class="flex-1 overflow-hidden flex flex-col mb-4">
             <div class="flex-1 overflow-auto space-y-4">
+                @php
+                    $isSingleRow = count($grid) === 1;
+                    $nonNullIndicators = $isSingleRow
+                        ? collect($grid[0])
+                            ->filter(fn($cell) => $cell['type'] === 'indicator' && isset($cell['data']))
+                            ->count()
+                        : 0;
+                @endphp
+
                 @foreach ($grid as $row)
-                    <div class="grid grid-cols-4 gap-4">
+                    <div class="grid {{ $isSingleRow ? "grid-cols-{$nonNullIndicators}" : 'grid-cols-4' }} gap-4">
                         @foreach ($row as $cell)
                             @if ($cell['type'] === 'indicator' && isset($cell['data']))
                                 <div>
